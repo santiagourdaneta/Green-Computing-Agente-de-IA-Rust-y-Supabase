234d332bd9283782c7bd1e0db826b2dc94a6f150
Santiago Urdaneta <santurdaneta@gmail.com>
2025-12-19 19:52:15 -0500

    fix: correcciones finales de clippy y rust

--- tests/integracion_ia.rs
+++ tests/integracion_ia.rs
@@ -3,9 +3,10 @@ async fn test_conexion_supabase_real() {
     dotenv::dotenv().ok();
     let client = reqwest::Client::new();
     let url = std::env::var("SUPABASE_URL").unwrap();
-    
+
     // Intentamos leer la tabla (sin importar si hay datos)
-    let res = client.get(format!("{}/rest/v1/conocimiento_agente?select=*", url))
+    let res = client
+        .get(format!("{}/rest/v1/conocimiento_agente?select=*", url))
         .header("apikey", std::env::var("SUPABASE_KEY").unwrap())
         .send()
         .await
